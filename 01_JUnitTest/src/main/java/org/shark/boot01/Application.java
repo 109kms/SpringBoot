1401f29935e952b62551e0f512ff42050d04d300
109kms <109kms@naver.com>
2025-09-15 17:48:44 +0900

    250915

--- 01_JUnitTest/src/main/java/org/shark/boot01/Application.java
+++ 01_JUnitTest/src/main/java/org/shark/boot01/Application.java
@@ -3,6 +3,16 @@ package org.shark.boot01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * @SpringBootApplication
+ * 
+ * 1. Spring Boot 애플리케이션의 메인 클래스에 추가하는 핵심 어노테이션입니다.
+ * 2. 3개의 어노테이션을 합쳐 놓은 메타 어노테이션입니다.
+ *    1) @Configuration
+ *    2) @EnableAutoConfiguration
+ *    3) @ComponentScan
+ */
+
 @SpringBootApplication
 public class Application {
 
