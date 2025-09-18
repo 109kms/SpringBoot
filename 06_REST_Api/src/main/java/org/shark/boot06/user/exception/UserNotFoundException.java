b58d04e42831affa42ef346e4a69a6f93baa3c26
109kms <109kms@naver.com>
2025-09-18 17:47:07 +0900

    250918

--- 06_REST_Api/src/main/java/org/shark/boot06/user/exception/UserNotFoundException.java
+++ 06_REST_Api/src/main/java/org/shark/boot06/user/exception/UserNotFoundException.java
@@ -2,6 +2,8 @@ package org.shark.boot06.user.exception;
 
 public class UserNotFoundException extends RuntimeException {
 
+  private static final long serialVersionUID = 7136258892847399314L;
+
   public UserNotFoundException(String message) {
     super(message);
   }
