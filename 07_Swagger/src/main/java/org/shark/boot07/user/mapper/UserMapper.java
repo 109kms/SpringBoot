1772b450b8a5a7a2250f59087b553db1940579d5
109kms <109kms@naver.com>
2025-09-19 17:04:55 +0900

    250919

---
+++ 07_Swagger/src/main/java/org/shark/boot07/user/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package org.shark.boot07.user.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.shark.boot07.user.dto.UserDTO;
+
+@Mapper
+public interface UserMapper {
+  Long selectUserCount();
+  List<UserDTO> selectUserList(@Param(value = "offset") Long offset
+                             , @Param(value = "size") int size
+                             , @Param(value = "sort") String sort);
+  UserDTO selectUserById(Long uid);
+  int insertUser(UserDTO user);
+  int updateUser(UserDTO user);
+  int deleteUser(Long uid);
+}
