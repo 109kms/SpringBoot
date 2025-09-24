1a57cb4da20b3b262847f0f86e8e114757dc7a88
109kms <109kms@naver.com>
2025-09-24 15:34:37 +0900

    250924

--- 11_Entity/src/test/java/org/shark/boot11/ApplicationTests.java
+++ 11_Entity/src/test/java/org/shark/boot11/ApplicationTests.java
@@ -1,13 +1,63 @@
 package org.shark.boot11;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.shark.boot11.common.util.JpaUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)  // @BeforeAll, @BeforeEach, @AfterEach, @AfterAll 메소드에 static 처리를 안 할 수 있도록 처리하는 Annotation
 class ApplicationTests {
+  
+  @Autowired
+  private JpaUtil jpaUtil;
+  
+  private EntityManager em;
+  
+  //----- 전체 테스트 시작 전 : 엔티티 매니저 팩토리 생성
+  @BeforeAll
+  void setUpBeforeClass() {
+    jpaUtil.initFactory();
+    log.info("@BeforeAll");
+  }
+  
+  //----- 각 테스트 시작 전 : 엔티티 매니저 생성
+  @BeforeEach
+  void setUp() {
+    em = jpaUtil.getEntityManager();
+    log.info("@BeforeEach");
+  }
+  
+  //----- 각 테스트 종료 후 : 엔티티 매니저 소멸
+  @AfterEach
+  void tearDown() {
+    if (em != null && em.isOpen()) em.close();
+    log.info("@AfterEach");
+  }
+  
+  //----- 전체 테스트 종료 휴 : 엔티티 매니저 팩토리 소멸
+  @AfterAll
+  void tearDownAfterClass() {
+    jpaUtil.closeFactory();
+    log.info("@AfterAll");
+  }
 
 	@Test
 	void contextLoads() {
+	  assertNotNull(em);
+	  log.info("contextLoads");
 	}
 
 }
